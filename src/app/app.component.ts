7f139d149734acb79362646060e7bab516620330
Andrés Gavilanes C <agavilanesc@outlook.com>
2020-07-19 03:20:14 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tyt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tyt-style-guides';
+}
